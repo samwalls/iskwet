61c0a1d0b71476852b3af48966c7e012c86f8525
Sam Walls <sam@s-w.io>
2020-05-19 01:16:42 +0100

    add basic API example

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+use rocket_contrib::uuid::Uuid;
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(Serialize, Deserialize)]
+struct Word {
+    uuid: String,
+    definitions: HashMap<String, String>,
+    dependencies: Vec<String>,
+    dependers: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Dictionary {
+    words: Vec<Word>,
+}
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/search/<uuid>", format = "json")]
+fn search_uuid(uuid: Uuid) -> String {
+    return "searching for '".to_string() + &uuid.to_string() + "'";
+}
+
+#[get("/search/<lang>/<word>", format = "json")]
+fn search_word(lang: String, word: String) -> String {
+    return "searching for '".to_string() + &word + "' in language: " + &lang;
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, search_uuid, search_word]).launch();
 }
