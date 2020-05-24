8d241c7bad2c711bd3dc4dfec0e03a2547df033b
Sam Walls <sam@s-w.io>
2020-05-24 23:50:40 +0100

    refactor search functions

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@
 use clap::{App, Arg};
 use rocket::State;
 use rocket_contrib::json::Json;
-use rocket_contrib::uuid::Uuid;
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::clone::Clone;
+use std::collections::HashMap;
 use std::fs;
+use std::iter::Iterator;
+use std::option::Option;
 
 #[macro_use]
 extern crate rocket;
@@ -36,10 +37,10 @@ impl Word {
             definitions: HashMap::new(),
             synonyms: HashMap::new(),
             antonyms: HashMap::new(),
-            dependencies: vec!(),
-            dependers: vec!(),
+            dependencies: vec![],
+            dependers: vec![],
             description: "".to_string(),
-        }
+        };
     }
 }
 
@@ -58,6 +59,20 @@ impl Dictionary {
         let dict: Dictionary = serde_json::from_str(&file_data).expect("could not parse JSON");
         return dict;
     }
+
+    fn find<P>(self, mut predicate: P) -> Option<Word>
+    where
+        P: FnMut(&Word) -> bool,
+    {
+        return self.words.into_iter().find(move |x| predicate(x));
+    }
+
+    fn find_all<P>(self, mut predicate: P) -> impl Iterator<Item = Word>
+    where
+        P: FnMut(&Word) -> bool,
+    {
+        return self.words.into_iter().filter(move |x| predicate(x));
+    }
 }
 
 #[get("/")]
@@ -67,18 +82,20 @@ fn index() -> &'static str {
 
 #[get("/search/<uuid>", format = "json")]
 fn search_uuid(dict: State<Dictionary>, uuid: String) -> Json<Word> {
-    let mut words = dict.words.clone().into_iter();
-    return match words.find(|word| word.uuid == uuid.to_string()) {
+    return match dict.clone().find(|word| word.uuid == uuid.to_string()) {
         Some(word) => Json(word),
         // TODO use results with errors
-        _ => Json(Word::new())
+        _ => Json(Word::new()),
     };
 }
 
 #[get("/search/<lang>/<word>", format = "json")]
 fn search_word(dict: State<Dictionary>, lang: String, word: String) -> Json<Vec<Word>> {
-    let words = dict.words.clone().into_iter();
-    return Json(words.filter(|word_data| word_data.definitions[&lang].contains(&word)).collect());
+    return Json(
+        dict.clone()
+            .find_all(|word_data| word_data.definitions[&lang].contains(&word))
+            .collect(),
+    );
 }
 
 fn main() {
@@ -86,10 +103,8 @@ fn main() {
         .version("0.1.0")
         .arg(
             Arg::with_name("data")
-                .short('d')
-                .long("data")
-                .takes_value(true)
                 .required(true)
+                .index(1)
                 .about("a path to a .json file containing dictionary + thesaurus data"),
         )
         .get_matches();
